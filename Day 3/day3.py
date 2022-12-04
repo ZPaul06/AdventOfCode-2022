d067e1ff19e767eb4da2cd2c7fc077a1b8104f77
ZPaul06 <paul_zwick@yahoo.com>
2022-12-04 15:21:54 +0100

    Simplified

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,8 +1,5 @@
 def getPriority(c):
-    if c.lower() != c:
-        return ord(c) - 38
-    else:
-        return ord(c) - 96
+    return ord(c) - 38 if c.lower() != c else ord(c) - 96
     
 
 with open("input.txt", "r") as f:
@@ -10,12 +7,8 @@ with open("input.txt", "r") as f:
     for line in f.read().splitlines():
         partA = line[0:int(len(line)/2)]
         partB = line[int(len(line)/2):int(len(line))]
-        sameItems = []
-        for c in partA:
-            if c in partB:
-                sameItems.append(c)
-        for c in set(sameItems):
-            prioritySum += getPriority(c)
+        sameItems = [c for c in partA if c in partB]
+        prioritySum += sum(getPriority(c) for c in set(sameItems))
 
     print("Part 1:  ", prioritySum)
 
@@ -23,15 +16,8 @@ with open("input.txt", "r") as f:
     prioritySum = 0
     lines = f.read().splitlines()
     for i in range(int(len(lines)/3)):
-        bagA = lines[3*i]
-        bagB = lines[3*i+1]
-        bagC = lines[3*i+2]
-        sameItems = []
-        for c in bagA:
-            if c in bagB and c in bagC:
-                sameItems.append(c)
-        for c in set(sameItems):
-            prioritySum += getPriority(c)
+        bagA, bagB, bagC = lines[3*i], lines[3*i+1], lines[3*i+2]
+        sameItems = [c for c in bagA if c in bagB and c in bagC]
+        prioritySum += sum(getPriority(c) for c in set(sameItems))
 
     print("Part 2:  ", prioritySum)
-                
