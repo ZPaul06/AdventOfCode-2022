dae16284b9ce404735802cde4b2b5e329beaca60
ZPaul06 <paul_zwick@yahoo.com>
2022-12-10 10:00:31 +0100

    Simplified

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -2,37 +2,27 @@ with open("input.txt", "r") as f:
     instructions = f.read().splitlines()
     register, cycleCounter = 1, 0
     signalStrengthSum = 0
+    screen = ["" for i in range(6)]
+    litPixel, darkPixel = "#", "."
     for instruction in instructions:
-        if instruction == "noop":
-            cycleCounter += 1
-            if cycleCounter % 40 == 20:
-                signalStrengthSum += cycleCounter * register
+        cycleCounter += 1
+        if cycleCounter % 40 == 20:
+            signalStrengthSum += cycleCounter * register
+        if (cycleCounter % 40) - 1 in range(register-1,register+2):
+            screen[int((cycleCounter-1)/40)] += litPixel
         else:
+            screen[int((cycleCounter-1)/40)] += darkPixel
+        if instruction.startswith("addx"):
             cycleCounter += 1
             if cycleCounter % 40 == 20:
                 signalStrengthSum += cycleCounter * register
-            cycleCounter += 1
-            if cycleCounter % 40 == 20:
-                signalStrengthSum += cycleCounter * register
+            if (cycleCounter % 40) - 1 in range(register-1,register+2):
+                screen[int((cycleCounter-1)/40)] += litPixel
+            else:
+                screen[int((cycleCounter-1)/40)] += darkPixel
             register += int(instruction[5:])
 
     print("Part 1:  ", signalStrengthSum)
-
-    register, cycleCounter = 1, 0
-    screen = ["" for i in range(6)]
-    for instruction in instructions:
-        cycleCounter += 1
-        if (cycleCounter % 40) - 1 in range(register-1,register+2):
-            screen[int((cycleCounter-1)/40)] += "#"
-        else:
-            screen[int((cycleCounter-1)/40)] += "."
-        if instruction.startswith("addx"):
-            cycleCounter += 1
-            if (cycleCounter % 40) - 1 in range(register-1,register+2):
-                screen[int((cycleCounter-1)/40)] += "#"
-            else:
-                screen[int((cycleCounter-1)/40)] += "."
-            register += int(instruction[5:])
     
     print("Part 2:")
     for row in screen: print(row)
